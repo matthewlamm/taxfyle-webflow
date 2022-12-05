4b1ef13d0280fea9a17120a3f320cb4ea5f14141
Matthew Lamm <matthew.lamm@taxfyle.com>
2022-12-05 09:13:12 -0700

    added version console.log

--- wf-complete-pro-application.js
+++ wf-complete-pro-application.js
@@ -1,6 +1,7 @@
 var progressBar = $("#progress-bar");
 //VALIDTE FORM ON PAGE LOAD
 $(document).ready(function () {
+  console.log('script v1');
   validateStep(1), validateStep(2), validateStep(3), validateStep(4), validateStep(5), moveProgressBar(currentStep);
 });
 //DROPDOWN FUNCTIONALITY
