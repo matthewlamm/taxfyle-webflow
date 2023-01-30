d07d7222f3e8be725c8a64e2ef2ff5aeb584dcdd
Matthew Lamm <matthew.lamm@taxfyle.com>
2023-01-30 13:12:23 -0700

    fixed bug in progress bar code

--- wf-complete-pro-application-v3.js
+++ wf-complete-pro-application-v3.js
@@ -251,8 +251,9 @@ $(".c-nav-dd_link").click(function () {
 function moveProgressBar(o) {
   if(o == 0){
     console.log("moving to step " + o), progressBar.css("transform", "translate(-" + (99 - 20 * o) + "%)");
+  }else{
+    console.log("moving to step " + o), progressBar.css("transform", "translate(-" + (100 - 20 * o) + "%)");
   }
-  console.log("moving to step " + o), progressBar.css("transform", "translate(-" + (100 - 20 * o) + "%)");
 }
 
 function saveToLocalStorage(inputs, selects){
